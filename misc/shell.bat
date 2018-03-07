b4d86a456732752ee688384eeae28c7cbcb838c4
Gyuhyeon, Lee <weanother@gmail.com>
2018-03-07 14:27:50 +0900

    what4

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,5 +1,5 @@
 @echo off
-rem This is for my notebook
+rem This is for my notebook, and this should not be updated!
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 
