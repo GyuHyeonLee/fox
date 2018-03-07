bd6db9dccb93120d64b2d1ebd284121a59e61cbd
Gyuhyeon, Lee <weanother@gmail.com>
2018-03-07 16:09:08 +0900

    deleting shell.bat

--- misc/shell.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem This is for my notebook, and this should not be updated!
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
-
-
-set path=W:\fox\misc;%path%
